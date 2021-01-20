d6d5a6e5ec9e6d754866c496fd892c3002d95c44
Pedro Costa <pedroftcosta95@gmail.com>
2021-01-20 21:01:47 +0000

    Create repository

---
+++ src/Moneybox.App/Domain/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Moneybox.App
+{
+    public class Account
+    {
+        public const decimal PayInLimit = 4000m;
+
+        public Guid Id { get; set; }
+
+        public User User { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public decimal Withdrawn { get; set; }
+
+        public decimal PaidIn { get; set; }
+
+        public void WithdrawFund(decimal amount)
+        {
+            if (this.Balance - amount < 0m)
+            {
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+            }
+
+            this.Balance -= amount;
+        }
+    }
+}
